53906c862237f703a8d00e16a40c343e6518eb72
Elias <eliasgalindo@gmail.com>
2021-12-10 17:00:40 -0300

    Requisito 9 feito - Clear Board

--- script.js
+++ script.js
@@ -8,18 +8,30 @@ function captureColor(e) {
 
 colorSelected.addEventListener('click', captureColor);
 
-function color(e) {
+function getColor(e) {
   const square = document.querySelector('.selected');
   const style = getComputedStyle(square);
   const background = style.backgroundColor;
   e.target.style.backgroundColor = background;
 }
 
-function colorFora() {
+function colorPixel() {
   const targetColor = document.querySelectorAll('.pixel');
   for (let index = 0; index < targetColor.length; index += 1) {
-    targetColor[index].addEventListener('click', color);
+    targetColor[index].addEventListener('click', getColor);
   }
 }
 
-colorFora();
+colorPixel();
+
+const buton = document.getElementById('clear-board');
+function clearBoard() {
+  const boardElements = document.querySelectorAll('.pixel');
+  for (let index = 0; index < boardElements.length; index += 1) {
+    boardElements[index].style.backgroundColor = 'rgb(255, 255, 255)';
+  }
+}
+
+buton.addEventListener('click', clearBoard);
+
+// Colaboração: Miguel Lima - Turma 19 - Tribo B
