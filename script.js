101230e7754492b361b101b3e10bd07d10224abd
Elias <eliasgalindo@gmail.com>
2021-12-10 11:09:51 -0300

    Requisito 8 feito - colore os pixels

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const colorSelected = document.querySelector('#color-palette');
 
-function captureColor(event) {
+function captureColor(e) {
   const classSelected = document.querySelector('.selected');
   classSelected.classList.remove('selected');
-  event.target.classList.add('selected');
+  e.target.classList.add('selected');
 }
 
 colorSelected.addEventListener('click', captureColor);
+
+function color(e) {
+  const square = document.querySelector('.selected');
+  const style = getComputedStyle(square);
+  const background = style.backgroundColor;
+  e.target.style.backgroundColor = background;
+}
+
+function colorFora() {
+  const targetColor = document.querySelectorAll('.pixel');
+  for (let index = 0; index < targetColor.length; index += 1) {
+    targetColor[index].addEventListener('click', color);
+  }
+}
+
+colorFora();
