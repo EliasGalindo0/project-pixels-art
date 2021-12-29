3f92156b9a416cd6abcc4ffb40a88e7c9d668856
Elias <eliasgalindo@gmail.com>
2021-12-29 00:16:57 -0300

    Requisito 12 feito - Cores randomicas

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const inputButton = document.getElementById('generate-board');
 const inputBoardValue = document.querySelector('#board-size');
 const valorStorage = localStorage.getItem('inputValue');
 const boardPixel = document.getElementById('pixel-board');
+const randomColors = document.querySelectorAll('.random');
 
 function getValueInput() {
   let aux = 0;
@@ -72,6 +73,26 @@ function boardSize() {
   }
   window.location.reload();
 }
+
+// https://www.ti-enxame.com/pt/javascript/gerador-de-cores-aleatorias/967183954/
+function randomColorsGenerate() {
+  const aux = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i += 1) {
+    color += aux[Math.floor(Math.random() * 16)];
+  }
+  return `${color}FF`;
+}
+
+function setColors() {
+  for (let i = 0; i < randomColors.length; i += 1) {
+    const color = randomColorsGenerate();
+    randomColors[i].style.backgroundColor = color;
+  }
+  return randomColors;
+}
+
+window.onload = setColors();
 
 inputButton.addEventListener('click', boardSize);
 buton.addEventListener('click', clearBoard);
